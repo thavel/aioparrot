37e8985654a1d6d4ca38ff3043ff2a93c8add320
Thibaut Havel <thibaut.havel@surycat.com>
2016-06-03 17:12:50 +0200

    Implement ARDrone actions and configs.

--- aioparrot/ardrone/factory.py
+++ aioparrot/ardrone/factory.py
@@ -3,11 +3,11 @@ from aioparrot.ardrone.utils import parrot_str
 
 def move(progressive):
     """
-    * left-right tilt (float): from -1 (left) to +1 (right)
-    * front-back tilt (float): from -1 (forwards) to +1 (backwards)
-    * vertical speed (float): from -1 (down) to +1 (up)
-    * angular speed (float): from -1 (spin left) to +1 (spin right)
-    + progressive (boolean): disable/enable (for hovering)
+    * left-right tilt (float): from -1 (left) to +1 (right).
+    * front-back tilt (float): from -1 (forwards) to +1 (backwards).
+    * vertical speed (float): from -1 (down) to +1 (up).
+    * angular speed (float): from -1 (spin left) to +1 (spin right).
+    + progressive (boolean): disable/enable (for hovering).
     """
     def wrapper(func):
         def call(*args, **kwargs):
@@ -18,27 +18,47 @@ def move(progressive):
     return wrapper
 
 
+def action(func):
+    """
+    * flag (binary): 10 bits flag.
+    """
+    def call(*args, **kwargs):
+        data = 0b10001010101000000000000000000
+        data += func(*args, **kwargs)
+        return args[0].format('REF', [data])
+    return call
+
+
 class CommandFactory(object):
     """
     ARDrone 1 and 2 controller.
     """
     def __init__(self, speed=0.1):
-        self.seq = 0
+        self._seq = 0
         self.speed = speed
 
+    @property
+    def get_seq(self):
+        self._seq += 1
+        return self._seq
+
     def format(self, command, data):
-        self.seq += 1
-        params = [str(self.seq)] + [parrot_str(value) for value in data]
+        params = [str(self.get_seq)] + [parrot_str(value) for value in data]
         return 'AT*{}={}\r'.format(command, ','.join(params))
 
-    def takeoff(self, *args, **kwargs):
-        raise NotImplementedError
+    def altitude(self, max=5000):
+        return self.format('CONFIG', ['control:altitude_max', str(max)])
 
-    def land(self, *args, **kwargs):
-        raise NotImplementedError
+    @action
+    def takeoff(self):
+        return 0b1000000000
 
-    def trim(self, *args, **kwargs):
-        raise NotImplementedError
+    @action
+    def land(self):
+        return 0b0000000000
+
+    def trim(self):
+        return self.format('FTRIM', [])
 
     @move(progressive=False)
     def hover(self):
@@ -76,5 +96,9 @@ class CommandFactory(object):
     def turn_right(self, unit=1):
         return [0, 0, 0, self.speed * unit]
 
-    def halt(self, *args, **kwargs):
-        raise NotImplementedError
+    @action
+    def emergency(self):
+        return 0b0100000000
+
+    def auto(self, start=True):
+        return self.format('AFLIGHT', [int(start)])
--- debug.py
+++ debug.py
@@ -13,6 +13,7 @@ async def main():
     log.critical("SDK version %s", drone.device.sdk)
 
     factory = CommandFactory()
+    log.critical("Altitude: %s", factory.altitude(3000))
     log.critical("Hover: %s", factory.hover())
     log.critical("Left: %s", factory.left())
     log.critical("Right: %s", factory.right())
@@ -22,6 +23,11 @@ async def main():
     log.critical("Up: %s", factory.up())
     log.critical("Turn left: %s", factory.turn_left())
     log.critical("Turn right: %s", factory.turn_right())
+    log.critical("Takeoff: %s", factory.takeoff())
+    log.critical("Land: %s", factory.land())
+    log.critical("Trim: %s", factory.trim())
+    log.critical("Auto on: %s", factory.auto())
+    log.critical("Auto off: %s", factory.auto(False))
 
 
 if __name__ == "__main__":
